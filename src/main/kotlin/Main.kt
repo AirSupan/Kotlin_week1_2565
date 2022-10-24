6182eaea3b896d6cce49f218e761efb2cf0a2bf5
AirSupan <airsupansa23@gmail.com>
2022-10-24 14:46:32 +0700

    first commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,18 @@
+fun main() {
+    kDataType()
+
+    println("#Any Type \n ==============")
+    val kAny1: Any = 20
+    val kAny2: Any = 100.4
+    val kAny3: Any = "Mark Zuckerberg"
+    println("kAny1: $kAny1")
+    println("kAny2: $kAny2")
+    println("kAny3: $kAny3")
+
+//    var a = 10 ; var b = 5 ; var c = a+b
+//    println(c)
+//    println("Hello Kotlin")
+//    println("สวัสดีทุกคน")
+//    println(12+12)
+//    println("เทอมนี้จะตั้งใจนอน)
+}
